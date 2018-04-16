c4abfee447f3aac480c483b8a26616ee26d3bff3
Michal Fojtak <mfojtak@seznam.cz>
2018-04-16 06:36:57 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python flask_app.py --slic --image_dir /images --label_names /labels.yml --file_ext jpg --prefix=/image-labelling $*
+python flask_app.py --slic --image_dir /images --label_names /labels.yaml --file_ext jpg --prefix=/image-labelling $*
