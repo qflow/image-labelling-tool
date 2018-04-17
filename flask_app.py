e46aba610b12c606663006055c2beb409d079df9
Michal Fojtak <mfojtak@seznam.cz>
2018-04-17 09:34:23 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
             img = plt.imread(path)
             # slic_labels = slic(img, 1000, compactness=20.0)
             # slic_labels = slic(img, 1000, slic_zero=True) + 1
-            slic_labels = slic(img, 1500, slic_zero=True) + 1
+            slic_labels = slic(img, 100, slic_zero=True) + 1
 
             print('Converting SLIC labels to vector labels...')
             labels = labelling_tool.ImageLabels.from_label_image(slic_labels)
